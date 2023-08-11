7288f40e6c028e25e341adbad9181edb442b1ca4
udit <udit@kvudit.com>
2023-08-10 21:04:33 -0700

    Generate dynamic invetory and add config file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
-output "ipv4" {
-    value=  proxmox_vm_qemu.kube-worker[0].default_ipv4_address	
-}
+# output "all-ips" {
+#     value=  proxmox_vm_qemu.*[*].default_ipv4_address	
+# }
+
+# output "control_planes" {
+#     value=  proxmox_vm_qemu.kube-control-plane[*].default_ipv4_address	
+# }
+# output "workers" {
+#     value=  proxmox_vm_qemu.kube-worker[*].default_ipv4_address	
+# }
+# output "gpu-nodes" {
+#     value=  proxmox_vm_qemu.kube-gpu-node[*].default_ipv4_address	
+# }
+# output "all-ips" {
+#     value=  proxmox_vm_qemu.kube-control-plane[0].default_ipv4_address	
+# }
+# output "all-ips" {
+#     value=  local.ssh_key
+# }
